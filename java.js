e1d218b1549eed204c5ff3b08bfcb51602f9cc1c
Ariel Cidral <arielkcidral1@gmail.com>
2026-07-05 12:07:29 -0300

    bahh

--- java.js
+++ java.js
@@ -106,7 +106,7 @@
   // Exemplo: 'assets/foto8.png'
   // Deixe null para manter o fundo em gradiente (sem imagem).
   const experienciaImages = [
-    null, // Central de Residuos
+    'assets/central_de_residuos.PNG', // Central de Residuos
     null, // Central de Cavacos
     null, // Fundicoes
     null, // Usinagem
